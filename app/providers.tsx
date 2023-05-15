43ad4a4c780f1b24b6e12f75aec05e47d4854422
blackPrawn <danblack9988@gmail.com>
2023-05-15 15:57:57 +0100

    install chakra ui and remove default content

---
+++ app/providers.tsx
@@ -0,0 +1,13 @@
+// app/providers.txs
+"use client";
+
+import { CacheProvider } from "@chakra-ui/next-js";
+import { ChakraProvider } from "@chakra-ui/react";
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <CacheProvider>
+      <ChakraProvider>{children}</ChakraProvider>
+    </CacheProvider>
+  );
+}
