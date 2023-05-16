12411425684d53dc24115014c104a2d52facc075
blackPrawn <danblack9988@gmail.com>
2023-05-16 09:39:00 +0100

    create navbar component

---
+++ components/layout/Navbar.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  Link,
+  Spacer,
+  useTheme,
+  IconButton,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  useDisclosure,
+  VStack,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+
+const Navbar = () => {
+  const theme = useTheme();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      padding="1rem"
+      bg="pictonBlue"
+      color="white"
+      px={[2, null, 8, null, 24]}
+    >
+      <VStack alignItems="flex-start">
+        <Text fontSize={["xl", null, "2xl", "4xl"]} color="white">
+          Scroll-a-dex!
+        </Text>
+        <Text fontSize="xl" color="white" whiteSpace="nowrap">
+          Your complete personnel directory
+        </Text>
+      </VStack>
+      <Spacer />
+      <HStack display={{ base: "none", md: "flex" }}>
+        <Button>Add employee</Button>
+        <Button>Add department</Button>
+      </HStack>
+      <Box display={{ base: "block", md: "none" }}>
+        <IconButton
+          aria-label="Open menu"
+          icon={<HamburgerIcon />}
+          onClick={onOpen}
+        />
+        <Drawer isOpen={isOpen} onClose={onClose} placement="right">
+          <DrawerOverlay />
+          <DrawerContent bg="blue">
+            <DrawerCloseButton />
+            <DrawerHeader>Menu</DrawerHeader>
+            <DrawerBody>
+              <Button>Add employee</Button>
+              <Button>Add department</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </Drawer>
+      </Box>
+    </Flex>
+  );
+};
+
+export default Navbar;
