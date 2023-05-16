9afe21f29731128644ef9849d9bdef176854e443
blackPrawn <danblack9988@gmail.com>
2023-05-16 09:38:43 +0100

    remove example css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme/theme";
+import Navbar from "@/components/layout/Navbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <Navbar />
       <Component {...pageProps} />
     </ChakraProvider>
   );
