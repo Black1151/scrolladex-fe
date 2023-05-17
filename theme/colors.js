c30a007f30f12424fbd0ccde12ed857374f55fba
blackPrawn <danblack9988@gmail.com>
2023-05-17 09:53:46 +0100

    add lighter shades of picton blue

--- theme/colors.js
+++ theme/colors.js
@@ -1,8 +1,10 @@
 const colors = {
-  bittersweet: "#ee6352ff",
-  emerald: "#59cd90ff",
-  pictonBlue: "#3fa7d6ff",
-  xanthous: "#fac05eff",
+  bittersweet: "#ee6352",
+  emerald: "#59cd90",
+  pictonBlue: "#3fa7d6",
+  lightPBlue: "#F2F9FC",
+  medPBlue: "#D2EAF6",
+  xanthous: "#fac05e",
 };
 
 export default colors;
