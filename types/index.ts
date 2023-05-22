bf6615020ffb41a4eae42c115f9aefaec34ff4e7
blackPrawn <danblack9988@gmail.com>
2023-05-22 07:48:43 +0100

    add type checking for employee and employee overview to types

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,18 @@ export type DepartmentListItem = {
     id: number;
     departmentName: string;
   };
+
+export interface Employee {
+    id?: number;
+    title: string;
+    firstName: string;
+    lastName: string;
+    empNo: string;
+    jobTitle: string;
+    departmentId: string;
+    telephone: string;
+    email: string;
+    profilePicture: File | Blob | null;
+  }
+
+export type EmployeeOverview = Pick<Employee, 'title' | 'firstName' | 'lastName' | 'jobTitle' | 'departmentId'> & { id: number };
