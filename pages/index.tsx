fdcaacd1544359f7dcdb67839a3c2ed5a926b8b1
blackPrawn <danblack9988@gmail.com>
2023-05-22 07:50:30 +0100

    create rudimentary employee overview and data fetching logic

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Box, Text, Heading, SimpleGrid } from "@chakra-ui/react";
+import { getEmployeesOverviewAPI } from "../api/employeeApi";
+import { EmployeeOverview } from "../types";
 
-const index = () => {
+const Index = () => {
+  const [employees, setEmployees] = useState<EmployeeOverview[]>([]);
+
+  useEffect(() => {
+    const fetchEmployees = async () => {
+      const data = await getEmployeesOverviewAPI();
+      if (data) {
+        setEmployees(data);
+      }
+    };
+
+    fetchEmployees();
+  }, []);
+
   return (
-    <div>index</div>
-  )
-}
+    <Box p={5}>
+      <Heading mb={5}>Employee Overview</Heading>
+      <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={5}>
+        {employees.map((employee) => (
+          <Box key={employee.id} p={5} shadow="md" borderWidth="1px">
+            <Heading fontSize="xl">
+              {employee.title} {employee.firstName} {employee.lastName}
+            </Heading>
+            <Text mt={2}>{employee.jobTitle}</Text>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+};
 
-export default index
+export default Index;
