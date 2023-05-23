5e1ea253bf3f37e63884a9ae7afa15d2e268b83d
blackPrawn <danblack9988@gmail.com>
2023-05-23 11:05:31 +0100

    update employee overview display with images

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Box, Text, Heading, SimpleGrid } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Box, Text, Heading, SimpleGrid, Flex, Image } from "@chakra-ui/react";
 import { getEmployeesOverviewAPI } from "../api/employeeApi";
 import { EmployeeOverview } from "../types";
 
@@ -17,16 +17,31 @@ const Index = () => {
     fetchEmployees();
   }, []);
 
+  // console.log(employees[0].profilePictureUrl);
+
   return (
     <Box p={5}>
       <Heading mb={5}>Employee Overview</Heading>
       <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} spacing={5}>
         {employees.map((employee) => (
           <Box key={employee.id} p={5} shadow="md" borderWidth="1px">
-            <Heading fontSize="xl">
-              {employee.title} {employee.firstName} {employee.lastName}
-            </Heading>
-            <Text mt={2}>{employee.jobTitle}</Text>
+            <Flex>
+              <Box>
+                <Heading fontSize="xl">
+                  {employee.title} {employee.firstName} {employee.lastName}
+                </Heading>
+                <Text mt={2}>{employee.jobTitle}</Text>
+              </Box>
+              <Box mr={4}>
+                <Image
+                  borderRadius="full"
+                  boxSize="100px"
+                  src={employee.profilePictureUrl || "/placeholder.png"}
+                  alt={employee.firstName + " " + employee.lastName}
+                  fallbackSrc="/placeholder.png"
+                />
+              </Box>
+            </Flex>
           </Box>
         ))}
       </SimpleGrid>
